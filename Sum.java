294e624a2214564ce28cd3bd5a0a9a18b34db895
rushabh782 <rushabhpatel877@gmail.com>
2024-03-17 14:15:24 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -1,9 +1,9 @@
-public class SumOfNumbers1  
+public class Sum  
 {  
 public static void main(String args[])   
 {  
 int n1 = 225, n2 = 115, sum;  
 sum = n1 + n2;  
-System.out.println("The sum of numbers is: "+sum);  
+System.out.println("The sum of two numbers is: "+sum);  
 }  
 }  
